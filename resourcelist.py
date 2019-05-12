a89c45b52f8ef90c0f2478ebac86a750633a1938
Tee Gobezie <teegobezie@gmail.com>
2019-05-12 02:11:51 -0400

    python code1

---
+++ resourcelist.py
@@ -0,0 +1,15 @@
+# This is a resouce list in aws script
+
+import boto3
+import os
+import datetime
+import math
+
+ec2 = boto3.resouce('ec2')
+s3 = boto3.resource('s3')
+
+for instance in ec2.instances.all():
+    print('Id:{} state:{} type:{}' .format(instance.id, instance.state, instance.instance_type))
+
+for bucket in s3.buckets.all():
+    print('Bucket name: {}' .format(bucket.name))
